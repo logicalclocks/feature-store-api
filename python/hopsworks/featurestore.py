df6a012fdfee2d80448f49f7803b80f8a812dfe6
moritzmeister <meister.mo@gmail.com>
2020-01-07 14:57:00 +0100

    Fix style in featurestore.py

--- python/hopsworks/featurestore.py
+++ python/hopsworks/featurestore.py
@@ -1,11 +1,11 @@
-import core
+from hopsworks import core
+
 
 class FeatureStore:
-
-    def __init__(name):
+    def __init__(self, name):
         self._name = name
-        self._id = # Find ID 
-        self._core = Core()
+        # self._id = # Find ID
+        self._core = core.Core()
 
     def feature_group(self, name, version):
         return self._core.feature_group(self, name, version)
