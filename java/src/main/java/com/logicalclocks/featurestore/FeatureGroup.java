e0e7f36649b12fec52581fa52ad57b82e1042342
Fabio Buso <buso.fabio@gmail.com>
2020-01-23 16:02:53 +0100

    more code removed

--- java/src/main/java/com/logicalclocks/featurestore/FeatureGroup.java
+++ java/src/main/java/com/logicalclocks/featurestore/FeatureGroup.java
@@ -1,25 +1,18 @@
 package com.logicalclocks.featurestore;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.logicalclocks.featurestore.engine.SparkEngine;
 import com.logicalclocks.featurestore.metadata.Query;
-import com.logicalclocks.featurestore.metadata.QueryConstructorApi;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FeatureGroup {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(FeatureGroup.class);
 
   @Getter @Setter
   private Integer id;
@@ -45,23 +38,15 @@ public class FeatureGroup {
   @Getter @Setter
   private List<Feature> features;
 
-  // API
-  private QueryConstructorApi queryConstructorApi;
-
-  public FeatureGroup(String name, Integer version, String description, Date created,
-                      String creator)
-      throws FeatureStoreException {
+  public FeatureGroup(String name, Integer version, String description, Date created, String creator) {
     this.name = name;
     this.version = version;
     this.description = description;
     this.created = created;
     this.creator = creator;
-
-    this.queryConstructorApi = new QueryConstructorApi();
   }
 
-  public FeatureGroup() throws FeatureStoreException {
-    this.queryConstructorApi = new QueryConstructorApi();
+  public FeatureGroup() {
   }
 
   public Dataset<Row> read() throws FeatureStoreException, IOException {
