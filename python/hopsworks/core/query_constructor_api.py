5dc69ca2c5b9fa25a89f5202093d045c35c92dbd
moritzmeister <meister.mo@gmail.com>
2020-03-20 11:43:58 +0100

    Remove redundant init in query api

--- python/hopsworks/core/query_constructor_api.py
+++ python/hopsworks/core/query_constructor_api.py
@@ -2,9 +2,6 @@ from hopsworks import client
 
 
 class QueryConstructorApi:
-    def __init__(self):
-        pass
-
     def construct_query(self, query):
         _client = client.get_instance()
         path_params = ["project", _client._project_id, "featurestores", "query"]
