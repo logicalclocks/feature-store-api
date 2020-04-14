d87e18a5548905d3f6134b254a134e8d2aa46d88
moritzmeister <meister.mo@gmail.com>
2020-04-14 15:39:34 +0200

    Reverse stupid remove

--- python/hopsworks/feature.py
+++ python/hopsworks/feature.py
@@ -3,9 +3,16 @@ import humps
 
 class Feature:
     def __init__(
-        self, name, description=None, primary=None, partition=None, online_type=None
+        self,
+        name,
+        type=None,
+        description=None,
+        primary=None,
+        partition=None,
+        online_type=None,
     ):
         self._name = name
+        self._type = type
         self._description = description
         self._primary = primary
         self._partition = partition
@@ -14,6 +21,7 @@ class Feature:
     def to_dict(self):
         return {
             "name": self._name,
+            "type": self._type,
             "description": self._description,
             "partition": self._partition,
             "primary": self._primary,
@@ -22,7 +30,6 @@ class Feature:
     @classmethod
     def from_response_json(cls, json_dict):
         json_decamelized = humps.decamelize(json_dict)
-        _ = json_decamelized.pop("type")
         return cls(**json_decamelized)
 
     @property
