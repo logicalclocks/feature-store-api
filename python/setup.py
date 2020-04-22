8002cfb65749be480b5db4a2067f50bf993c1451
moritzmeister <meister.mo@gmail.com>
2020-04-22 18:21:56 +0200

    Change License

--- python/setup.py
+++ python/setup.py
@@ -35,7 +35,7 @@ setup(
     author="Moritz Meister",
     author_email="moritz@logicalclocks.com",
     description="",
-    license="GNU Affero General Public License v3",
+    license="Apache License 2.0",
     keywords="",
     url="",
     download_url="",
