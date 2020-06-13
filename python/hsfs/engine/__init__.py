171cb5cf8395d544656df45f089679165e7d7b7d
Fabio Buso <buso.fabio@gmail.com>
2020-06-14 00:53:38 +0200

    fix engine init

--- python/hsfs/engine/__init__.py
+++ python/hsfs/engine/__init__.py
@@ -19,13 +19,13 @@ from hsfs.engine import spark, hive
 _engine = None
 
 
-def init(engine_type, host=None, cert_folder=None, cert_key=None):
+def init(engine_type, host=None, cert_folder=None, project=None, cert_key=None):
     global _engine
     if not _engine:
         if engine_type == "spark":
             _engine = spark.Engine()
         elif engine_type == "hive":
-            _engine = hive.Engine(host, cert_folder, cert_key)
+            _engine = hive.Engine(host, cert_folder, project, cert_key)
 
 
 def get_instance():
