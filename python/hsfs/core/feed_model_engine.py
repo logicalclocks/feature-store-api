92adfc27699ca6b22d76deb1d64dd5b215d97602
davitbzh <davit@logicalclocks.com>
2020-07-02 19:45:07 +0200

    Feed tfrecord tf2

--- python/hsfs/core/feed_model_engine.py
+++ python/hsfs/core/feed_model_engine.py
@@ -1,8 +1,5 @@
 try:
-    import numpy as np
     import tensorflow as tf
-    from petastorm import make_reader
-    from petastorm.tf_utils import make_petastorm_dataset
     from pydoop import hdfs
 except ModuleNotFoundError:
     pass
@@ -16,27 +13,25 @@ class FeedModelEngine:
         self,
         training_dataset,
         split,
-        target_name,
+        target_names=None,
         feature_names=None,
-        one_hot_encode_labels=False,
-        num_classes=None,
+        is_training=True,
+        cycle_length=2,
     ):
         """
-
         :param training_dataset:
         :param split:
         :param target_name:
         :param feature_names:
-        :param one_hot_encode_labels:
-        :param num_classes:
+        :param is_training:
         """
 
         self.training_dataset = training_dataset
         self.split = split
-        self.target_name = target_name
+        self.target_names = target_names
         self.feature_names = feature_names
-        self.one_hot_encode_labels = one_hot_encode_labels
-        self.num_classes = num_classes
+        self.is_training = is_training
+        self.cycle_length = cycle_length
 
         self.training_dataset_schema = self.training_dataset.schema
 
@@ -49,19 +44,18 @@ class FeedModelEngine:
 
         if self.feature_names is None:
             self.feature_names = [feat.name for feat in self.training_dataset_schema]
-            if self.target_name in self.feature_names:
-                self.feature_names.remove(self.target_name)
 
-        self.tf_record_schema = create_tfrecord_feature_description(
-            self.training_dataset_schema
-        )
+            for target_name in self.target_names:
+                if target_name in self.feature_names:
+                    self.feature_names.remove(target_name)
 
-        if self.one_hot_encode_labels and self.num_classes is None:
-            raise ValueError(
-                "if one_hot_encode_labels is set to True you also need to provide num_classes"
-            )
-
-    def tf_record_dataset(self, batch_size=None, num_epochs=None, optimize=False):
+    def tf_record_dataset(
+        self,
+        batch_size=None,
+        num_epochs=None,
+        optimize=False,
+        serialized_ndarray_fname=[],
+    ):
         """
         :param batch_size:
         :param num_epochs:
@@ -75,118 +69,53 @@ class FeedModelEngine:
             )
 
         input_files = tf.io.gfile.glob(self.path + "/part-r-*")
-        dataset = tf.data.TFRecordDataset(input_files)
 
-        def _decode(sample):
-            example = tf.io.parse_single_example(sample, self.tf_record_schema)
+        dataset = tf.data.Dataset.from_tensor_slices(input_files)
+
+        # Convert to individual records.
+        # cycle_length = 2 means that up to 2 files will be read and deserialized in
+        # parallel. You may want to increase this number if you have a large number of
+        # CPU cores.
+        dataset = dataset.interleave(
+            tf.data.TFRecordDataset,
+            cycle_length=self.cycle_length,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+
+        tfrecord_feature_description = create_tfrecord_feature_description(dataset)
+
+        def _de_serialize(serialized_example):
+            example = tf.io.parse_single_example(
+                serialized_example, tfrecord_feature_description
+            )
             x = []
             for feature_name in self.feature_names:
-                x.append(example[feature_name])
+                if feature_name in serialized_ndarray_fname:
+                    x.append(
+                        tf.io.parse_tensor(example[feature_name], out_type=tf.float64)
+                    )
+                else:
+                    x.append(example[feature_name])
 
-            # TODO (davit): provide user ability to enter tf.dtypes.DType
-            y = [tf.cast(example[self.target_name], tf.float32)]
-            return x, y
+            y = []
+            for target_name in self.target_names:
+                y.append(example[target_name])
+            return tuple(x), tuple(y)
 
-        dataset = dataset.map(_decode)
-
-        if self.one_hot_encode_labels:
-            dataset = dataset.map(
-                lambda *x: _one_hot_encode(
-                    x[0], tf.dtypes.cast(x[1], tf.int32), self.num_classes
-                )
-            )
-
-        if optimize:
-            dataset = _optimize_dataset(dataset, batch_size, num_epochs)
-
-        return dataset
-
-    def numpy(self):
-        """
-
-        :return:
-        """
-        tfdata = self.tf_record_dataset()
-
-        iterator = tfdata.make_one_shot_iterator().get_next()
-        features = []
-        target = []
-        if tf.__version__ >= "2.0":
-            # TODO (davit): make also for tf2
-            raise NotImplementedError
-        else:
-            with tf.compat.v1.Session() as sess:
-                while True:
-                    # import psutil
-                    # if psutil.virtual_memory() >= 84:
-                    #     break
-                    try:
-                        batch_array = sess.run(iterator)
-                        features.append(batch_array[0])
-                        target.append(batch_array[1])
-                    except tf.errors.OutOfRangeError:
-                        break
-        features = np.array(features)
-        target = np.array(target).flatten()
-        return features, target
-
-    # TODO  (davit): this function is work in progress
-    def tf_petastorm_dataset(
-        self,
-        workers_count,
-        shuffle_row_groups,
-        batch_size=None,
-        num_epochs=None,
-        optimize=False,
-    ):
-        """
-
-        :param workers_count:
-        :param shuffle_row_groups:
-        :param batch_size:
-        :param num_epochs:
-        :param optimize:
-        :return:
-        """
-
-        with make_reader(
-            self.path,
-            num_epochs=None,
-            hdfs_driver="libhdfs",
-            workers_count=workers_count,
-            shuffle_row_groups=shuffle_row_groups,
-        ) as reader:
-            dataset = make_petastorm_dataset(reader)
-
-            def _row_mapper(sample):
-                out_dict = dict()
-
-                for feature in self.feature_names:
-                    if hasattr(sample, feature):
-                        out_dict[feature] = getattr(sample, feature)
-                    else:
-                        print("{} is not known".format(feature))
-
-                label = getattr(sample, self.target_name)
-
-                return out_dict, label
-
-            dataset = dataset.map(_row_mapper)
-
-        if self.one_hot_encode_labels:
-            dataset = dataset.map(
-                lambda *x: _one_hot_encode(
-                    x[0], tf.dtypes.cast(x[1], tf.int32), self.num_classes
-                )
-            )
+        dataset = dataset.map(
+            lambda value: _de_serialize(value),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
 
         if optimize:
-            dataset = _optimize_dataset(dataset, batch_size, num_epochs)
+            dataset = _optimize_dataset(
+                dataset, batch_size, num_epochs, self.is_training
+            )
 
         return dataset
 
 
-def _optimize_dataset(dataset, batch_size, num_epochs):
+def _optimize_dataset(dataset, batch_size, num_epochs, is_training):
     """
 
     :param dataset:
@@ -195,20 +124,12 @@ def _optimize_dataset(dataset, batch_size, num_epochs):
     :return:
     """
     # tf data optimization
-    dataset = dataset.repeat()  # num_epochs * steps_per_epoch
+    if is_training:
+        dataset = dataset.shuffle(num_epochs * batch_size)
+        dataset = dataset.repeat()  # num_epochs * steps_per_epoch
     dataset = dataset.cache()
-    dataset = dataset.shuffle(num_epochs * batch_size)
     dataset = dataset.batch(batch_size, drop_remainder=True)
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-    # options = tf.data.Options()
-    #
-    # options.experimental_stats.latency_all_edges = True
-    # options.experimental_optimization.noop_elimination = True
-    # options.experimental_optimization.map_vectorization.enabled = True
-    # options.experimental_optimization.apply_default_optimizations = False
-    # # options.experimental_threading.private_threadpool_size = 10
-    # dataset = dataset.with_options(options)
 
     return dataset
 
--- python/hsfs/tf_utils.py
+++ python/hsfs/tf_utils.py
@@ -4,40 +4,32 @@ except ModuleNotFoundError:
     pass
 
 
-def create_tfrecord_feature_description(df_schema, fixed=True):
+def create_tfrecord_feature_description(dataset):
 
-    """
-    :param df_schema: dataframe schema to infer the tensorflow example record from
-    :param fixed: if feature is fixed or not
-    :return: tfrecord feature description
-    """
-
-    # supported type mappings between DataFrame.dtypes and tf.train.Feature types
-    float_dtypes = ["float", "double"]
-    int64_dtypes = ["integer", "long"]
-    bytes_dtypes = ["binary", "string"]
-
-    bytes_list_dtypes = ["array<binary>", "array<string>"]
-    float_list_dtypes = ["array<float>", "array<double>"]
-    int64_list_dtypes = ["array<integer>", "array<long>", "array<float>"]
+    for raw_record in dataset.take(1):
+        example = tf.train.Example()
+        example.ParseFromString(raw_record.numpy())
 
     feature_description = {}
 
-    for col in df_schema:
-        if col.type in float_dtypes or col.type in float_list_dtypes:
-            feature_description[col.name] = tf.io.FixedLenFeature([], tf.float32)
-        elif col.type in int64_dtypes or col.type in int64_list_dtypes:
-            feature_description[col.name] = tf.io.FixedLenFeature([], tf.int64)
-        elif col.type in bytes_dtypes or col.type in bytes_list_dtypes:
-            feature_description[col.name] = tf.io.FixedLenFeature([], tf.string)
-        else:
-            raise InferTFRecordSchemaError(
-                "Could not recognize type {} for inferring the tf-records schema.".format(
-                    col.type
-                )
-            )
+    for k, v in sorted(example.features.feature.items()):
+        f_name, f_description = _infer_tf_dtype(k, v)
+        feature_description[f_name] = f_description
+
     return feature_description
 
 
-class InferTFRecordSchemaError(Exception):
-    """This exception will be raised if there is an error in inferring the tfrecord schema of a dataframe"""
+def _infer_tf_dtype(k, v):
+
+    if v.int64_list.value:
+        result = v.int64_list.value
+        feature_length = len(result)
+        feature_type = tf.io.FixedLenFeature([feature_length], tf.int64)
+    elif v.float_list.value:
+        result = v.float_list.value
+        feature_length = len(result)
+        feature_type = tf.io.FixedLenFeature([feature_length], tf.float32)
+    elif v.bytes_list.value:
+        feature_type = tf.io.FixedLenFeature([], tf.string)
+
+    return k, feature_type
