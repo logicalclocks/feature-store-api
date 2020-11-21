6515daaf9bfc0b18f3877d25eff8bf4216b09675
Fabio Buso <buso.fabio@gmail.com>
2020-11-21 01:25:31 +0100

    add utility methods

--- utils/hsfs_utils.py
+++ utils/hsfs_utils.py
@@ -3,7 +3,8 @@ import json
 import hsfs
 
 from typing import Dict, Any
-from pydoop import hdfs 
+from pydoop import hdfs
+from pyspark.sql import SparkSession
 
 
 def read_job_conf(path: str) -> Dict[Any, Any]:
@@ -11,17 +12,50 @@ def read_job_conf(path: str) -> Dict[Any, Any]:
     return json.loads(file_content)
 
 
+def setup_spark() -> SparkSession:
+    return SparkSession.builder.enableHiveSupport().getOrCreate()
+
+
 def get_feature_store_handle(feature_store: str = "") -> hsfs.feature_store:
     connection = hsfs.connection()
     return connection.get_feature_store(feature_store)
 
 
-def create_td(job_conf: Dict[Any, Any]) -> None:
+def create_fg(spark: SparkSession, job_conf: Dict[Any, Any]) -> None:
     feature_store = job_conf.pop("feature_store")
     fs = get_feature_store_handle(feature_store)
 
+    data_path = job_conf.pop("data_path")
+    data_format = job_conf.pop("data_format")
+    data_options = job_conf.pop("data_options")
 
-    fs.create_training_dataset()
+    df = spark.read.format(data_format).options(data_options).load(data_path)
+
+    fg = fs.create_feature_group(**job_conf)
+    fg.save(df)
+
+
+def create_td(job_conf: Dict[Any, Any]) -> None:
+    # Extract the feature store handle
+    feature_store = job_conf.pop("feature_store")
+    fs = get_feature_store_handle(feature_store)
+
+    # Extract the query object
+    query = hsfs.Query.from_response_json(job_conf.pop("query"))
+
+    storage_connector = None
+    if "storage_connector" in job_conf:
+        storage_connector_json = job_conf.pop("storage_connector")
+        storage_connector = fs.get_storage_connector(
+            storage_connector_json["name"], storage_connector_json["type"]
+        )
+
+    td = fs.create_training_dataset(**job_conf, storage_connector=storage_connector)
+    td.save(query)
+
+
+def compute_stats(job_conf: Dict[Any, Any]) -> None:
+    pass
 
 
 if __name__ == "__main__":
@@ -41,8 +75,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     job_conf = read_job_conf(args.path)
 
+    spark = setup_spark()
+
     if args.op == "create_fg":
-        create_fg(job_conf)
+        create_fg(spark, job_conf)
     elif args.op == "create_td":
         create_td(job_conf)
     elif args.op == "compute_stats":
