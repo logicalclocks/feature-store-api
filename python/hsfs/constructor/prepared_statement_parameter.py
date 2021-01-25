befcf430badcd8983398242239f91f55d75fb62f
davitbzh <davit@logicalclocks.com>
2021-01-25 16:50:00 +0100

    serving_vector_keys

--- python/hsfs/constructor/prepared_statement_parameter.py
+++ python/hsfs/constructor/prepared_statement_parameter.py
@@ -11,15 +11,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#
-#  http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
 
 import humps
 import json
@@ -29,7 +20,6 @@ from hsfs import util
 
 class PreparedStatementParameter:
     def __init__(self, name=None, index=None, type=None, href=None):
-
         self._name = name
         self._index = index
 
