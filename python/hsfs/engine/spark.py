73638570bb485a54583fd9090e79aacadb270d33
Ralf <bubriks@gmail.com>
2021-10-08 14:15:30 +0200

    Update spark.py

--- python/hsfs/engine/spark.py
+++ python/hsfs/engine/spark.py
@@ -180,7 +180,10 @@ class Engine:
             self._save_online_dataframe(feature_group, dataframe, online_write_options)
         elif online_enabled and storage is None:
             self._save_offline_dataframe(
-                feature_group, dataframe, operation, offline_write_options,
+                feature_group,
+                dataframe,
+                operation,
+                offline_write_options,
             )
             self._save_online_dataframe(feature_group, dataframe, online_write_options)
         else:
@@ -232,7 +235,12 @@ class Engine:
         return query
 
     def _save_offline_dataframe(
-        self, feature_group, dataframe, operation, write_options, validation_id=None,
+        self,
+        feature_group,
+        dataframe,
+        operation,
+        write_options,
+        validation_id=None,
     ):
         if feature_group.time_travel_format == "HUDI":
             hudi_engine_instance = hudi_engine.HudiEngine(
@@ -449,12 +457,14 @@ class Engine:
         exact_uniqueness=True,
     ):
         """Profile a dataframe with Deequ."""
-        return self._jvm.com.logicalclocks.hsfs.engine.SparkEngine.getInstance().profile(
-            dataframe._jdf,
-            relevant_columns,
-            correlations,
-            histograms,
-            exact_uniqueness,
+        return (
+            self._jvm.com.logicalclocks.hsfs.engine.SparkEngine.getInstance().profile(
+                dataframe._jdf,
+                relevant_columns,
+                correlations,
+                histograms,
+                exact_uniqueness,
+            )
         )
 
     def validate(self, dataframe, expectations):
@@ -621,7 +631,8 @@ class Engine:
                 "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider",
             )
             self._spark_context._jsc.hadoopConfiguration().set(
-                "fs.s3a.session.token", storage_connector.session_token,
+                "fs.s3a.session.token",
+                storage_connector.session_token,
             )
         return path.replace("s3", "s3a", 1) if path is not None else None
 
