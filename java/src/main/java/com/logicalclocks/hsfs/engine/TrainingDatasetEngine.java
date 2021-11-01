7bab85cdf60baba15031691cc98afa5a62c9c66d
davitbzh <44586065+davitbzh@users.noreply.github.com>
2021-11-01 11:06:26 +0100

    Update java/src/main/java/com/logicalclocks/hsfs/engine/TrainingDatasetEngine.java
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- java/src/main/java/com/logicalclocks/hsfs/engine/TrainingDatasetEngine.java
+++ java/src/main/java/com/logicalclocks/hsfs/engine/TrainingDatasetEngine.java
@@ -300,7 +300,7 @@ public class TrainingDatasetEngine {
   }
 
   public List<List<Object>> getServingVectors(TrainingDataset trainingDataset, Map<String, List<Object>> entry,
-                                        boolean external) throws SQLException, FeatureStoreException, IOException,
+                                              boolean external) throws SQLException, FeatureStoreException, IOException,
           ClassNotFoundException {
 
     // init prepared statement if it has not already
