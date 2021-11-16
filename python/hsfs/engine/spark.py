095f8b7144567ff778cbbc764eac143e2f428f8e
Fabio Buso <fabio@logicalclocks.com>
2021-11-16 13:54:03 +0100

    Enable PySpark session should have Hive support enabled (#528)

--- python/hsfs/engine/spark.py
+++ python/hsfs/engine/spark.py
@@ -45,7 +45,7 @@ class Engine:
     OVERWRITE = "overwrite"
 
     def __init__(self):
-        self._spark_session = SparkSession.builder.getOrCreate()
+        self._spark_session = SparkSession.builder.enableHiveSupport().getOrCreate()
         self._spark_context = self._spark_session.sparkContext
         self._jvm = self._spark_context._jvm
 
