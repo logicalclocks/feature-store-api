0f66798af43c7dd45161d3236edb404d6d34daa3
kennethmhc <kennethmhc@users.noreply.github.com>
2022-01-26 11:02:30 +0100

    [HOPSWORKS-2948] Fix import error (#555)

--- python/hsfs/connection.py
+++ python/hsfs/connection.py
@@ -16,12 +16,11 @@
 
 import os
 import importlib.util
-import util
 
 from requests.exceptions import ConnectionError
 
 from hsfs.decorators import connected, not_connected
-from hsfs import engine, client
+from hsfs import engine, client, util
 from hsfs.core import feature_store_api, project_api, hosts_api, services_api, rules_api
 
 AWS_DEFAULT_REGION = "default"
