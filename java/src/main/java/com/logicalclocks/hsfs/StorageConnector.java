2cd2d07135ab11ec8ca77afc7225fec1487c7be3
Dhananjay <dhananjay1098@gmail.com>
2022-02-17 19:25:44 +0100

    fix as per master-remove kafka related

--- java/src/main/java/com/logicalclocks/hsfs/StorageConnector.java
+++ java/src/main/java/com/logicalclocks/hsfs/StorageConnector.java
@@ -187,7 +187,7 @@ public abstract class StorageConnector {
     private String iamRole;
 
     @Getter @Setter
-    private List<Option> arguments;
+    private String arguments;
 
     @Getter @Setter
     private Instant expiration;
@@ -195,10 +195,8 @@ public abstract class StorageConnector {
     public Map<String, String> sparkOptions() {
       String constr =
           "jdbc:redshift://" + clusterIdentifier + "." + databaseEndpoint + ":" + databasePort + "/" + databaseName;
-      if (arguments != null && !arguments.isEmpty()) {
-        constr += "?" + arguments.stream()
-          .map(arg -> arg.getName() + (arg.getValue() != null ? "=" + arg.getValue() : ""))
-          .collect(Collectors.joining(","));
+      if (!Strings.isNullOrEmpty(arguments)) {
+        constr += "?" + arguments;
       }
       Map<String, String> options = new HashMap<>();
       options.put(Constants.JDBC_DRIVER, databaseDriver);
