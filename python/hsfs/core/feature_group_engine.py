339a629792584073624e4435577ae994a67cd801
Fabio Buso <buso.fabio@gmail.com>
2022-05-31 14:59:13 +0200

    Fix update description for stream feature groups

--- python/hsfs/core/feature_group_engine.py
+++ python/hsfs/core/feature_group_engine.py
@@ -179,6 +179,7 @@ class FeatureGroupEngine(feature_group_base_engine.FeatureGroupBaseEngine):
             featurestore_id=None,
             description=None,
             id=feature_group.id,
+            stream=feature_group.stream,
             features=features,
         )
         self._feature_group_api.update_metadata(
