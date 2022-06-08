33ecdfaaf4dc9be6174df518006c373b42bb0992
Robin Andersson <robin.eric.andersson@gmail.com>
2022-06-08 15:07:00 +0200

    Additions to setup.py to fix great_expectations issues in 0.14.3 (#662)

--- python/setup.py
+++ python/setup.py
@@ -29,7 +29,9 @@ setup(
         "sqlalchemy",
         "PyMySQL[rsa]",
         "great_expectations==0.14.3",
-        "jinja2==3.0.3",
+        "jinja2==2.11.3", # GE issue 1: great_expectations pulls in jinja 3.1.2 which causes import of great_expectations to fail
+        "markupsafe==2.0.1", # GE issue 2: jinja2==2.11.3, pulls in markupsafe 2.1.0 which is not compatible with jinja2==2.11.3
+        "typing_extensions>=3.7.4", # GE issue 3: missing dependency https://github.com/great-expectations/great_expectations/pull/4082/files, set to 3.7.4 to be compatible with hopsworks base environment
     ],
     extras_require={
         "dev": ["pytest", "flake8", "black"],
