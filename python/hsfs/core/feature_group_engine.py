92834042160b5f1a287ccfc9c662c04ec86f7c54
moritzmeister <moritz@logicalclocks.com>
2022-06-22 14:00:02 +0200

    remove sleep after fix in kafka authorizer

--- python/hsfs/core/feature_group_engine.py
+++ python/hsfs/core/feature_group_engine.py
@@ -13,7 +13,6 @@
 #   limitations under the License.
 #
 import warnings
-import time
 
 from hsfs import engine, client, util
 from hsfs import feature_group as fg
@@ -33,9 +32,6 @@ class FeatureGroupEngine(feature_group_base_engine.FeatureGroupBaseEngine):
         self._save_feature_group_metadata(
             feature_group, feature_dataframe, write_options
         )
-
-        # sleep to make sure authorizer added kafka topic
-        time.sleep(1.2)
 
         # deequ validation only on spark
         validation = feature_group._data_validation_engine.ingest_validate(
