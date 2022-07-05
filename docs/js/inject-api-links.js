9ae17374b0c72f05bf7e21024a9ff4aa9de027b2
moritzmeister <moritz@logicalclocks.com>
2022-07-05 14:20:11 +0200

    add tutorials

--- docs/js/inject-api-links.js
+++ docs/js/inject-api-links.js
@@ -15,9 +15,10 @@ window.addEventListener("DOMContentLoaded", function () {
 
         document.getElementsByClassName("md-tabs__link")[0].href = "https://docs.hopsworks.ai/" + majorVersion;
         document.getElementsByClassName("md-tabs__link")[1].href = "https://docs.hopsworks.ai/" + majorVersion + "/getting_started/";
-        document.getElementsByClassName("md-tabs__link")[2].href = "https://docs.hopsworks.ai/" + majorVersion + "/concepts/hopsworks/";
-        document.getElementsByClassName("md-tabs__link")[3].href = "https://docs.hopsworks.ai/" + majorVersion + "/user_guides/"
-        document.getElementsByClassName("md-tabs__link")[4].href = "https://docs.hopsworks.ai/" + majorVersion; + "/setup_installation/aws/getting_started/"
-        document.getElementsByClassName("md-tabs__link")[5].href = "https://docs.hopsworks.ai/" + majorVersion; + "/admin/"
+        document.getElementsByClassName("md-tabs__link")[2].href = "https://docs.hopsworks.ai/" + majorVersion + "/tutorials/";
+        document.getElementsByClassName("md-tabs__link")[3].href = "https://docs.hopsworks.ai/" + majorVersion + "/concepts/hopsworks/";
+        document.getElementsByClassName("md-tabs__link")[4].href = "https://docs.hopsworks.ai/" + majorVersion + "/user_guides/"
+        document.getElementsByClassName("md-tabs__link")[5].href = "https://docs.hopsworks.ai/" + majorVersion; + "/setup_installation/aws/getting_started/"
+        document.getElementsByClassName("md-tabs__link")[6].href = "https://docs.hopsworks.ai/" + majorVersion; + "/admin/"
     }
 });
