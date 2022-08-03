a402892c2453e315fd4b458d741b08a3bb50e4b9
Ralf <bubriks@gmail.com>
2022-08-03 16:38:16 +0200

    Delete test.py

--- python/test.py
+++
@@ -1,8 +0,0 @@
-from python.hsfs.engine.python import Engine as pythonEngine
-from python.hsfs.engine.spark import Engine as sparkEngine
-
-p = pythonEngine()
-s = sparkEngine()
-
-p.register_external_temporary_table(None, None)
-print(1)
