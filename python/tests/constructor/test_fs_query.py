fbff015acfdff839381ca99d2b5a16a56f87c866
Ralf <bubriks@gmail.com>
2022-08-16 14:16:49 +0200

    fs_query tests

---
+++ python/tests/constructor/test_fs_query.py
@@ -0,0 +1,51 @@
+#
+#   Copyright 2022 Hopsworks AB
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+
+from hsfs.constructor import hudi_feature_group_alias, external_feature_group_alias, fs_query
+from hsfs import feature_group
+
+class TestFsQuery:
+
+    def test_from_response_json(self, backend_fixtures):
+        # Arrange
+        json = backend_fixtures["get_fs_query"]["response"]
+
+        # Act
+        q = fs_query.FsQuery.from_response_json(json)
+
+        # Assert
+        assert q.query == "test_query"
+        assert len(q._on_demand_fg_aliases) == 1
+        assert isinstance(q._on_demand_fg_aliases[0], external_feature_group_alias.ExternalFeatureGroupAlias)
+        assert len(q._hudi_cached_feature_groups) == 1
+        assert isinstance(q._hudi_cached_feature_groups[0], hudi_feature_group_alias.HudiFeatureGroupAlias)
+        assert q.query_online == "test_query_online"
+        assert q.pit_query == "test_pit_query"
+
+    def test_from_response_json_basic_info(self, backend_fixtures):
+        # Arrange
+        json = backend_fixtures["get_fs_query_basic_info"]["response"]
+
+        # Act
+        q = fs_query.FsQuery.from_response_json(json)
+
+        # Assert
+        assert q.query == "test_query"
+        assert len(q._on_demand_fg_aliases) == 0
+        assert len(q._hudi_cached_feature_groups) == 0
+        assert q.query_online == None
+        assert q.pit_query == None
