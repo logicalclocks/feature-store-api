95228b09f0ec3b9cccfbed184bd341c89c33c8bc
Ralf <bubriks@gmail.com>
2022-08-17 15:26:05 +0200

    training_dataset_feature tests

---
+++ python/tests/test_training_dataset_feature.py
@@ -0,0 +1,53 @@
+#
+#   Copyright 2022 Hopsworks AB
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+
+from hsfs import training_dataset_feature, feature_group, transformation_function
+
+
+class TestTrainingDatasetFeature:
+    def test_from_response_json(self, backend_fixtures):
+        # Arrange
+        json = backend_fixtures["get_training_dataset_feature"]["response"]
+
+        # Act
+        td_feature = training_dataset_feature.TrainingDatasetFeature.from_response_json(json)
+
+        # Assert
+        assert td_feature.name == "test_name"
+        assert td_feature.type == "test_type"
+        assert td_feature.index == "test_index"
+        assert isinstance(td_feature._feature_group, feature_group.FeatureGroup)
+        assert td_feature._feature_group_feature_name == "test_feature_group_feature_name"
+        assert td_feature.label == "test_label"
+        assert len(td_feature.transformation_function) == 1
+        assert isinstance(td_feature.transformation_function[0], transformation_function.TransformationFunction)
+
+    def test_from_response_json_basic_info(self, backend_fixtures):
+        # Arrange
+        json = backend_fixtures["get_training_dataset_feature_basic_info"]["response"]
+
+        # Act
+        td_feature = training_dataset_feature.TrainingDatasetFeature.from_response_json(json)
+
+        # Assert
+        assert td_feature.name == "test_name"
+        assert td_feature.type == None
+        assert td_feature.index == None
+        assert td_feature._feature_group == None
+        assert td_feature._feature_group_feature_name == None
+        assert td_feature.label == False
+        assert td_feature.transformation_function == None
