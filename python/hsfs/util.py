4a91ca742dad3ab38ec909419d51933263a742ea
davitbzh <44586065+davitbzh@users.noreply.github.com>
2022-10-31 10:37:19 +0100

    Update python/hsfs/util.py
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- python/hsfs/util.py
+++ python/hsfs/util.py
@@ -222,7 +222,7 @@ def get_hostname_replaced_url(sub_path: str):
 
 
 def verify_attribute_key_names(feature_group_obj):
-    feature_names = set([feat.name for feat in feature_group_obj.features])
+    feature_names = set(feat.name for feat in feature_group_obj.features)
     if feature_group_obj.primary_key:
         diff = set(feature_group_obj.primary_key) - feature_names
         if diff:
