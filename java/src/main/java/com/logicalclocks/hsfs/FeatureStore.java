e342380a76020f2f3203d04353d83b6792874cab
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-02-20 16:46:33 +0100

    Update java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
+++ java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
@@ -141,7 +141,7 @@ public class FeatureStore {
    * Get a stream feature group object from the feature store.
    *
    * <p>Getting a stream feature group from the Feature Store means getting its metadata handle so you can subsequently
-   * read the data into a Spark  DataFrame or use the `Query`-API to perform joins between feature groups.
+   * read the data into a Spark DataFrame or use the `Query`-API to perform joins between feature groups and create feature views.
    *
    * <pre>
    * {@code //get feature store handle
