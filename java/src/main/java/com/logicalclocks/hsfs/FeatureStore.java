4fd562971683fa0901163320069e13b1213145fc
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-02-20 20:17:49 +0100

    Update java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
+++ java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
@@ -377,7 +377,7 @@ public class FeatureStore {
    * </pre>
    *
    * @param name Name of the storage connector to retrieve.
-   * @return StorageConnector.RedshiftConnector: Storage connector object
+   * @return StorageConnector.RedshiftConnector Storage connector object.
    * @throws FeatureStoreException If unable to retrieve StorageConnector from the feature store.
    * @throws IOException If ...
    */
