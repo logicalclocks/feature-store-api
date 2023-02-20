36c55fef37e5546051843cc19b0638211b15e5ef
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-02-20 14:34:03 +0100

    Update java/src/main/java/com/logicalclocks/hsfs/FeatureGroup.java
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- java/src/main/java/com/logicalclocks/hsfs/FeatureGroup.java
+++ java/src/main/java/com/logicalclocks/hsfs/FeatureGroup.java
@@ -299,9 +299,9 @@ public class FeatureGroup extends FeatureGroupBase {
    *
    * <pre>
    * {@code
-   *        //get feature store handle
+   *        // get feature store handle
    *        FeatureStore fs = ...;
-   *        //get feature group handle
+   *        // get feature group handle
    *        FeatureGroup fg = ...;
    *        // get query object to retrieve all feature group as of specific point in time.
    *        fg.asOf("20230205210923")
