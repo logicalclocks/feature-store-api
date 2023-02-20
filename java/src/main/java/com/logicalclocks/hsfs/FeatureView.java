767a81584970cf4ce815caa4bd218c7a030fb5e7
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-02-20 21:57:48 +0100

    Update java/src/main/java/com/logicalclocks/hsfs/FeatureView.java
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- java/src/main/java/com/logicalclocks/hsfs/FeatureView.java
+++ java/src/main/java/com/logicalclocks/hsfs/FeatureView.java
@@ -420,11 +420,11 @@ public class FeatureView {
    *
    * <pre>
    * {@code
-   *        //get feature store handle
+   *        // get feature store handle
    *        FeatureStore fs = ...;
-   *        //get feature view handle
+   *        // get feature view handle
    *        FeatureView fv = ...;
-   *        //Get batch query
+   *        // get batch query
    *        fv.getBatchQuery();
    * }
    * </pre>
