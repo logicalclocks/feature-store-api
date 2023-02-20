723032b9aa6cccc49255dba7141bceb1a617ff58
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-02-20 21:25:41 +0100

    Update java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
    Co-authored-by: Moritz Meister <8422705+moritzmeister@users.noreply.github.com>

--- java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
+++ java/src/main/java/com/logicalclocks/hsfs/FeatureStore.java
@@ -779,9 +779,9 @@ public class FeatureStore {
    * }
    * </pre>
    *
-   * @param name name of the feature view
-   * @param query Query object
-   * @param version version of the feature view
+   * @param name Name of the feature view.
+   * @param query Query object.
+   * @param version Version of the feature view.
    * @return FeatureView The feature view metadata object.
    * @throws FeatureStoreException If unable to retrieve FeatureView from the feature store.
    * @throws IOException Generic IO exception.
