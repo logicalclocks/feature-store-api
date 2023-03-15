90696ed633e2ce2ca4d5318f16b9c1e96c049903
Victor Jouffrey <37411285+vatj@users.noreply.github.com>
2023-03-15 22:30:50 +0100

    [FSTORE-721] sketch prototype run feature monitoring job (#949)
    * Draft
    
    * Added endpoints to config api + minor fixes
    
    * Test doc
    
    * Documentation
    
    * Cleaned up entity id passed around to every call
    
    * Fix setup job endpoint
    
    * Fix repr function
    
    * Fix result api
    
    * Fix monitoring time setting
    
    * test query params fix
    
    * Ensure monitoring time cannot be more precise than what the db can handle
    
    * round monitoring time
    
    * job_name
    
    * Issue with circular import
    
    * Fix to get config by name in run feature monitoring job
    
    * More modification to run_feature_monitoring job
    
    * Fix mismatch naming issue
    
    * Time offset and monitoring time dummy
    
    * Fix start and end time + dataframe type (temporary fix)
    
    * Fixes to run feature monitoring job
    
    * Some doc rewritting
    
    * minor fix
    
    * Additional fix to result engine
    
    * Typo...
    
    * Fix fake dict for specific value
    
    * lower case metric
    
    * Try feature view support
    
    * Hack for featureview
    
    * Hack for featureview
    
    * Moving to spark
    
    * fix dict
    
    * fix the specific value
    
    * Ditch setup job rest call before config creation
    
    * Add unit test for stats only config
    
    * minor cleanup
    
    * Scheduled statistics
    
    * Fix entity type + tidy up
    
    * Fixed missing reference stats
    
    * Address code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Dhananjay Mukhedkar <55157590+DhananjayMukhedkar@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dhananjay Mukhedkar <55157590+DhananjayMukhedkar@users.noreply.github.com>

--- python/hsfs/core/feature_monitoring_result_api.py
+++ python/hsfs/core/feature_monitoring_result_api.py
@@ -14,46 +14,54 @@
 #   limitations under the License.
 #
 
-from typing import List, Optional
+from typing import Dict, List, Optional, Union
 from hsfs import client
 from hsfs.core.feature_monitoring_result import FeatureMonitoringResult
 
 
 class FeatureMonitoringResultApi:
-    def __init__(self, feature_store_id: int):
-        """Feature Monitoring Result endpoints for the Feature Group resource.
+    """Feature Monitoring Result endpoints for the Feature Group resource.
 
-        :param feature_store_id: id of the respective Feature Store
-        :type feature_store_id: int
-        """
+    :param feature_store_id: id of the respective Feature Store
+    :type feature_store_id: int
+    :param feature_group_id: id of the feature group, if attaching a config to a feature group
+    :type feature_group_id: int, optional
+    :param feature_view_name: name of the feature view, if attaching a config to a feature view
+    :type feature_view_name: str, optional
+    :param feature_view_version: version of the feature view, if attaching a config to a feature view
+    :type feature_view_version: int, optional
+    """
+
+    def __init__(
+        self,
+        feature_store_id: int,
+        feature_group_id: Optional[int] = None,
+        feature_view_name: Optional[str] = None,
+        feature_view_version: Optional[int] = None,
+    ):
+        if feature_group_id is None:
+            assert feature_view_name is not None
+            assert feature_view_version is not None
+
         self._feature_store_id = feature_store_id
+        self._feature_group_id = feature_group_id
+        self._feature_view_name = feature_view_name
+        self._feature_view_version = feature_view_version
 
     def create(
         self,
         fm_result: FeatureMonitoringResult,
-        feature_group_id: Optional[int] = None,
-        feature_view_name: Optional[str] = None,
-        feature_view_version: Optional[int] = None,
     ) -> FeatureMonitoringResult:
         """Create an feature monitoring result attached to the Feature of a Feature Group.
 
         :param fm_result: feature monitoring result object to be attached to a Feature
         :type fm_result: `FeatureMonitoringResult`
-        :param feature_group_id: id of the feature group, if attaching a config to a feature group
-        :type feature_group_id: int, optional
-        :param feature_view_name: name of the feature view, if attaching a config to a feature view
-        :type feature_view_name: str, optional
-        :param feature_view_version: version of the feature view, if attaching a config to a feature view
-        :type feature_view_version: int, optional
         :return: the created feature monitoring result
         :rtype: FeatureMonitoringResult
         """
         _client = client.get_instance()
         path_params = self.build_path_params(
             project_id=_client._project_id,
-            feature_group_id=feature_group_id,
-            feature_view_name=feature_view_name,
-            feature_view_version=feature_view_version,
         )
 
         headers = {"content-type": "application/json"}
@@ -65,25 +73,11 @@ class FeatureMonitoringResultApi:
     def delete(
         self,
         result_id: int,
-        feature_group_id: Optional[int] = None,
-        feature_view_name: Optional[str] = None,
-        feature_view_version: Optional[int] = None,
     ) -> None:
-        """Delete the Feature Monitoring result attached to a Feature.
-
-        :param feature_group_id: id of the feature group, if attaching a config to a feature group
-        :type feature_group_id: int, optional
-        :param feature_view_name: name of the feature view, if attaching a config to a feature view
-        :type feature_view_name: str, optional
-        :param feature_view_version: version of the feature view, if attaching a config to a feature view
-        :type feature_view_version: int, optional
-        """
+        """Delete the Feature Monitoring result attached to a Feature."""
         _client = client.get_instance()
         path_params = self.build_path_params(
             project_id=_client._project_id,
-            feature_group_id=feature_group_id,
-            feature_view_name=feature_view_name,
-            feature_view_version=feature_view_version,
         )
         path_params.append(result_id)
 
@@ -92,63 +86,43 @@ class FeatureMonitoringResultApi:
     def get_by_config_id(
         self,
         config_id: int,
-        feature_group_id: Optional[int] = None,
-        feature_view_name: Optional[str] = None,
-        feature_view_version: Optional[int] = None,
+        query_params: Optional[Dict[str, Union[str, List[str]]]] = None,
     ) -> List[FeatureMonitoringResult]:
-        """Get the Feature Monitoring Result attached to a Feature.
+        """Get all Feature Monitoring Result attached to a Monitoring Config.
 
         :param config_id: Id of the feature monitoring config for which to fetch all results
         :type config_id: int
-        :param feature_group_id: id of the feature group, if attaching a config to a feature group
-        :type feature_group_id: int, optional
-        :param feature_view_name: name of the feature view, if attaching a config to a feature view
-        :type feature_view_name: str, optional
-        :param feature_view_version: version of the feature view, if attaching a config to a feature view
-        :type feature_view_version: int, optional
+        :param query_params: query parameters to filter the results by monitoring_time
+        :type query_params: Dict[str, Union[str, List[str]]], optional
         :return: fetched feature monitoring results attached to the Feature Group
         :rtype: List[FeatureMonitoringResult]
         """
         _client = client.get_instance()
         path_params = self.build_path_params(
             project_id=_client._project_id,
-            feature_group_id=feature_group_id,
-            feature_view_name=feature_view_name,
-            feature_view_version=feature_view_version,
         )
         path_params.append("byconfig")
         path_params.append(config_id)
+        headers = {"content-type": "application/json"}
 
         return FeatureMonitoringResult.from_response_json(
-            _client._send_request("GET", path_params)
+            _client._send_request("GET", path_params, query_params, headers=headers)
         )
 
     def get_by_id(
         self,
         result_id: int,
-        feature_group_id: Optional[int] = None,
-        feature_view_name: Optional[str] = None,
-        feature_view_version: Optional[int] = None,
     ) -> List[FeatureMonitoringResult]:
         """Get the Feature Monitoring Result attached to a Feature.
 
         :param result_id: Id of the feature monitoring result to fetch
         :type result_id: int
-        :param feature_group_id: id of the feature group, if attaching a config to a feature group
-        :type feature_group_id: int, optional
-        :param feature_view_name: name of the feature view, if attaching a config to a feature view
-        :type feature_view_name: str, optional
-        :param feature_view_version: version of the feature view, if attaching a config to a feature view
-        :type feature_view_version: int, optional
         :return: fetched feature monitoring result attached to the Feature Group
         :rtype: FeatureMonitoringResult || None
         """
         _client = client.get_instance()
         path_params = self.build_path_params(
             project_id=_client._project_id,
-            feature_group_id=feature_group_id,
-            feature_view_name=feature_view_name,
-            feature_view_version=feature_view_version,
         )
         path_params.append(result_id)
 
@@ -159,21 +133,30 @@ class FeatureMonitoringResultApi:
     def build_path_params(
         self,
         project_id: int,
-        feature_group_id: Optional[int] = None,
-        feature_view_name: Optional[str] = None,
-        feature_view_version: Optional[int] = None,
     ) -> List[str]:
+        """Build the path parameters for the Feature Monitoring Result API.
+
+        :param project_id: Id of the project
+        :type project_id: int
+        :return: list of path parameters
+        :rtype: List[str]
+        """
         path_params = [
             "project",
             project_id,
             "featurestores",
             self._feature_store_id,
         ]
-        if feature_group_id is not None:
-            path_params.extend(["featuregroups", feature_group_id])
+        if self._feature_group_id is not None:
+            path_params.extend(["featuregroups", self._feature_group_id])
         else:
             path_params.extend(
-                ["featureview", feature_view_name, "version", feature_view_version]
+                [
+                    "featureview",
+                    self._feature_view_name,
+                    "version",
+                    self._feature_view_version,
+                ]
             )
         path_params.extend(["featuremonitoring", "result"])
 
