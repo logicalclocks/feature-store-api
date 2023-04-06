06d9683873a15e4cb83e613d698b5fd4a92ffd8b
Victor Jouffrey <vicatjou@gmail.com>
2023-04-06 22:02:53 +0200

    Add some documentation for monitoring window config

--- python/hsfs/core/monitoring_window_config.py
+++ python/hsfs/core/monitoring_window_config.py
@@ -41,6 +41,68 @@ class MonitoringWindowConfig:
         specific_value: Optional[float] = None,
         row_percentage: Optional[int] = None,
     ):
+        """Configuration to define the slice of data to compute statistics on.
+
+        !!! example
+            ```python3
+            # Detection or reference window
+            ## Rolling Time Window
+            monitoring_window_config = MonitoringWindowConfig(
+                time_offset="1d", # data inserted up to 1 day ago
+                window_length="1h", # data inserted until an one hour after time_offset
+                row_percentage=20, # include only 20% of the rows when computing statistics
+            )
+
+            ## Number Of Commits (not supported yet)
+            monitoring_window_config = MonitoringWindowConfig(
+                commit_offset=10, # data inserted up to 10 commits ago
+                commit_num=5, # include 5 commits after commit_offset
+            )
+
+            # Only available for reference window
+            ## Specific value
+            monitoring_window_config = MonitoringWindowConfig(specific_value=0.5)
+
+            ## Training dataset
+            monitoring_window_config = MonitoringWindowConfig(
+                specific_id=my_training_dataset.id
+            )
+
+            ## Rolling Snapshot (not supported yet)
+            monitoring_window_config = MonitoringWindowConfig(
+                time_offset="LATEST", # Use the latest available snapshot statistics
+            )
+
+            ## Fix Time Window (not supported yet)
+            monitoring_window_config = MonitoringWindowConfig(
+                time_offset="2020-01-01 00:00:00", # data inserted up to 2020-01-01 00:00:00
+                window_length="1y", # data inserted until an one year after time_offset
+            )
+            ```
+
+        # Arguments
+            id: int, optional
+                The id of the monitoring window config.
+            window_config_type: str, optional
+                The type of the monitoring window config. One of INSERT, SNAPSHOT,
+                BATCH, TRAINING_DATASET, SPECIFIC_VALUE.
+            time_offset: str, optional
+                The time offset of the monitoring window config. Only used for
+                INSERT and SNAPSHOT window config types.
+            window_length: str, optional
+                The window length of the monitoring window config. Only used for
+                INSERT and SNAPSHOT window config types.
+            specific_id: int, optional
+                The id of the training dataset to use as reference. Only used for
+                TRAINING_DATASET window config type.
+            specific_value: float, optional
+                The specific value to use as reference. Only used for SPECIFIC_VALUE
+                window config type.
+
+        # Raises
+            AttributeError: If window_config_type is not one of INSERT, SNAPSHOT,
+                BATCH, TRAINING_DATASET, SPECIFIC_VALUE.
+        """
         self._id = id
         self._window_config_type = window_config_type
         self._time_offset = time_offset
@@ -88,7 +150,7 @@ class MonitoringWindowConfig:
         return self._id
 
     @property
-    def window_config_type(self) -> Optional[str]:
+    def window_config_type(self) -> WindowConfigType:
         return self._window_config_type
 
     @property
@@ -99,13 +161,46 @@ class MonitoringWindowConfig:
     def window_length(self) -> Optional[str]:
         return self._window_length
 
+    @window_length.setter
+    def window_length(self, window_length: Optional[str]):
+        if (
+            self._window_config_type != WindowConfigType.INSERT
+            and window_length is not None
+        ):
+            raise AttributeError(
+                "Window length can only be set for if window_config_type is INSERT."
+            )
+        self._window_length = window_length
+
     @property
     def specific_id(self) -> Optional[int]:
         return self._specific_id
 
+    @specific_id.setter
+    def specific_id(self, specific_id: Optional[int]):
+        if (
+            self._window_config_type != WindowConfigType.TRAINING_DATASET
+            and specific_id is not None
+        ):
+            raise AttributeError(
+                "Specific id can only be set for if window_config_type is TRAINING_DATASET."
+            )
+        self._specific_id = specific_id
+
     @property
     def specific_value(self) -> Optional[float]:
         return self._specific_value
+
+    @specific_value.setter
+    def specific_value(self, specific_value: Optional[float]):
+        if (
+            self._window_config_type != WindowConfigType.SPECIFIC_VALUE
+            and specific_value is not None
+        ):
+            raise AttributeError(
+                "Specific value can only be set for if window_config_type is SPECIFIC_VALUE."
+            )
+        self._specific_value = specific_value
 
     @property
     def row_percentage(self) -> Optional[int]:
