ce0245466cca0d0576b2a9cb9afb32ae13d22c4d
davitbzh <44586065+davitbzh@users.noreply.github.com>
2023-05-16 14:56:09 +0200

    [FSTORE-751][APPEND] add external kafka listeners for flink engine (#997)
    Co-authored-by: Fabio Buso <buso.fabio@gmail.com>

--- java/flink/src/main/java/com/logicalclocks/hsfs/flink/engine/FlinkEngine.java
+++ java/flink/src/main/java/com/logicalclocks/hsfs/flink/engine/FlinkEngine.java
@@ -20,6 +20,7 @@ package com.logicalclocks.hsfs.flink.engine;
 import com.logicalclocks.hsfs.FeatureStoreException;
 import com.logicalclocks.hsfs.metadata.HopsworksClient;
 import com.logicalclocks.hsfs.metadata.HopsworksHttpClient;
+import com.logicalclocks.hsfs.metadata.HopsworksInternalClient;
 import com.logicalclocks.hsfs.metadata.KafkaApi;
 import com.logicalclocks.hsfs.flink.StreamFeatureGroup;
 
@@ -43,7 +44,7 @@ import java.util.stream.Collectors;
 public class FlinkEngine {
   private static FlinkEngine INSTANCE = null;
 
-  public static synchronized FlinkEngine getInstance() {
+  public static synchronized FlinkEngine getInstance() throws FeatureStoreException {
     if (INSTANCE == null) {
       INSTANCE = new FlinkEngine();
     }
@@ -53,19 +54,20 @@ public class FlinkEngine {
   @Getter
   private StreamExecutionEnvironment streamExecutionEnvironment;
 
-  private KafkaApi kafkaApi = new KafkaApi();
+  private final KafkaApi kafkaApi = new KafkaApi();
+  private final HopsworksHttpClient client = HopsworksClient.getInstance().getHopsworksHttpClient();
 
-  private FlinkEngine() {
+  private FlinkEngine() throws FeatureStoreException {
     streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
     // Configure the streamExecutionEnvironment
     streamExecutionEnvironment.getConfig().enableObjectReuse();
   }
 
-  public DataStreamSink<?> writeDataStream(StreamFeatureGroup streamFeatureGroup, DataStream<?> dataStream)
-      throws FeatureStoreException, IOException {
+  public DataStreamSink<?> writeDataStream(StreamFeatureGroup streamFeatureGroup, DataStream<?> dataStream,
+      Map<String, String> writeOptions) throws FeatureStoreException, IOException {
 
     DataStream<Object> genericDataStream = (DataStream<Object>) dataStream;
-    Properties properties = getKafkaProperties(streamFeatureGroup);
+    Properties properties = getKafkaProperties(streamFeatureGroup, writeOptions);
 
     KafkaSink<GenericRecord> sink = KafkaSink.<GenericRecord>builder()
         .setBootstrapServers(properties.getProperty("bootstrap.servers"))
@@ -94,12 +96,28 @@ public class FlinkEngine {
     return avroRecordDataStream.sinkTo(sink);
   }
 
-  private Properties getKafkaProperties(StreamFeatureGroup featureGroup) throws FeatureStoreException, IOException {
-    HopsworksHttpClient client = HopsworksClient.getInstance().getHopsworksHttpClient();
+  private Properties getKafkaProperties(StreamFeatureGroup featureGroup, Map<String, String> writeOptions)
+      throws FeatureStoreException, IOException {
+
     Properties properties = new Properties();
-    properties.put("bootstrap.servers",
-        kafkaApi.getBrokerEndpoints(featureGroup.getFeatureStore()).stream().map(broker -> broker.replaceAll(
-        "INTERNAL://", "")).collect(Collectors.joining(",")));
+    boolean internalKafka = false;
+    if (writeOptions != null) {
+      internalKafka = Boolean.parseBoolean(writeOptions.getOrDefault("internal_kafka", "false"));
+      properties.putAll(writeOptions);
+    }
+
+    if (System.getProperties().containsKey(HopsworksInternalClient.REST_ENDPOINT_SYS) || internalKafka) {
+      properties.put("bootstrap.servers",
+          kafkaApi.getBrokerEndpoints(featureGroup.getFeatureStore()).stream().map(broker -> broker.replaceAll(
+          "INTERNAL://", ""))
+            .collect(Collectors.joining(",")));
+    } else {
+      properties.put("bootstrap.servers",
+          kafkaApi.getBrokerEndpoints(featureGroup.getFeatureStore(), true).stream()
+            .map(broker -> broker.replaceAll("EXTERNAL://", ""))
+            .collect(Collectors.joining(","))
+      );
+    }
     properties.put("security.protocol", "SSL");
     properties.put("ssl.truststore.location", client.getTrustStorePath());
     properties.put("ssl.truststore.password", client.getCertKey());
