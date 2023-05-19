50cd45318075b829ddc12197dba3a6326bab291a
Victor Jouffrey <37411285+vatj@users.noreply.github.com>
2023-05-19 15:34:43 +0200

    [FSTORE-848] Adding more unit tests (#1019)
    * Add use_event_time and transformation_function_dataset_version
    
    * Add raised_exception, empty_window, dataset_version and use_event_time
    
    * Refactor monitoring result engine
    
    * Add fetch data unit test
    
    * Add unittest with row percentage sampling
    
    * Refactoring monitoring window specific function to corresponding engine
    
    * Add build monitoring result unit tests
    
    * Test compute differences
    
    * Add run_and_save_comparison test
    
    * Remove need for result engine in feature monitoring job
    
    * Add type hint to feature store
    
    * Some more type hints and imports
    
    * Pass feature name if available when registering result with raised exception
    
    * Address review comments

--- python/hsfs/feature_view.py
+++ python/hsfs/feature_view.py
@@ -2483,6 +2483,8 @@ class FeatureView:
         feature_name: Optional[str] = None,
         description: Optional[str] = None,
         start_date_time: Optional[Union[int, str, datetime, date, pd.Timestamp]] = None,
+        use_event_time: Optional[bool] = True,
+        training_dataset_version: Optional[int] = None,
     ) -> "fmc.FeatureMonitoringConfig":
         """Run a job to compute statistics on snapshot of feature data on a schedule.
 
@@ -2516,6 +2518,12 @@ class FeatureView:
                 Options are "HOURLY", "DAILY", "WEEKLY", "MONTHLY", defaults to "DAILY".
             description: Description of the feature monitoring configuration.
             start_date_time: Start date and time from which to start computing statistics.
+            use_event_time: If true, use event time to compute statistics.
+                Defaults to False.
+            training_dataset_version: The version of the dataset to use
+                to fetch statistics for the transformation function. If provided, the
+                statistics are computed after applying the transformation function.
+                Otherwise, the statistics are computed on the raw data. Defaults to None.
 
         # Raises
             `hsfs.client.exceptions.FeatureStoreException`.
@@ -2535,6 +2543,8 @@ class FeatureView:
             description=description,
             job_frequency=job_frequency,
             start_date_time=start_date_time,
+            use_event_time=use_event_time,
+            training_dataset_version=training_dataset_version,
             valid_feature_names=[feat.name for feat in self._features],
         )
 
@@ -2545,6 +2555,8 @@ class FeatureView:
         job_frequency: str = "DAILY",
         description: Optional[str] = None,
         start_date_time: Optional[Union[int, str, datetime, date, pd.Timestamp]] = None,
+        use_event_time: Optional[bool] = True,
+        training_dataset_version: Optional[int] = None,
     ) -> "fmc.FeatureMonitoringConfig":
         """Enable feature monitoring to compare statistics on snapshots of feature data over time.
 
@@ -2583,6 +2595,12 @@ class FeatureView:
                 Options are "HOURLY", "DAILY", "WEEKLY", "MONTHLY", defaults to "DAILY".
             description: Description of the feature monitoring configuration.
             start_date_time: Start date and time from which to start computing statistics.
+            use_event_time: If true, use event time to compute statistics.
+                Defaults to False.
+            training_dataset_version: The version of the dataset to use
+                to fetch statistics for the transformation function. If provided, the
+                statistics are computed after applying the transformation function.
+                Otherwise, the statistics are computed on the raw data. Defaults to None
 
         # Raises
             `hsfs.client.exceptions.FeatureStoreException`.
@@ -2602,6 +2620,8 @@ class FeatureView:
             description=description,
             job_frequency=job_frequency,
             start_date_time=start_date_time,
+            use_event_time=use_event_time,
+            training_dataset_version=training_dataset_version,
             valid_feature_names=[feat.name for feat in self._features],
         )
 
@@ -2744,12 +2764,12 @@ class FeatureView:
         self._description = description
 
     @property
-    def query(self):
+    def query(self) -> "query.Query":
         """Query of the feature view."""
         return self._query
 
     @query.setter
-    def query(self, query_obj):
+    def query(self, query_obj: "query.Query"):
         self._query = query_obj
 
     @property
