d376451f88784baaee17f7abb25ecdda055cd9d8
Till Döhmen <till.doehmen@web.de>
2023-06-08 10:10:36 +0200

    [FSTORE-855] Query object refactoring and docs (#1015)
    * working state
    
    * refactored query parameters
    
    * removed numeric parameter
    
    * fix bug
    
    * look in all features in all featuregroups
    
    * support excluded features
    
    * added features property to featureview
    
    * include transformation functions in repr
    
    * fix column check on join
    
    * updated warning messages
    
    * more refactoring, fixed tests
    
    * fixed tests, added filter check
    
    * fixed test
    
    * fixed ambiguity issue with filter params
    
    * fixed tests and cleanup
    
    * check full filter
    
    * fix append feature and check filter there as well
    
    * don't check merged filter on .filter
    
    * working state
    
    * removed checks
    
    * some renaming
    
    * made lookup function private/protected
    
    * added docs
    
    * fixed cached check
    
    * updated error messages, reverted transformation function test changes
    
    * added tests
    
    * added add_features_to_collection method
    
    * collect features on request
    
    * fixed comment

--- python/tests/constructor/test_query.py
+++ python/tests/constructor/test_query.py
@@ -13,13 +13,58 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-
-
+import pytest
 from hsfs import feature_group, feature
 from hsfs.constructor import query, join, filter
+from hsfs.client.exceptions import FeatureStoreException
 
 
 class TestQuery:
+    fg1 = feature_group.FeatureGroup(
+        name="test1",
+        version=1,
+        featurestore_id=99,
+        primary_key=[],
+        partition_key=[],
+        features=[
+            feature.Feature("id", feature_group_id=11),
+            feature.Feature("label", feature_group_id=11),
+            feature.Feature("tf_name", feature_group_id=11),
+        ],
+        id=11,
+        stream=False,
+    )
+
+    fg2 = feature_group.FeatureGroup(
+        name="test2",
+        version=1,
+        featurestore_id=99,
+        primary_key=[],
+        partition_key=[],
+        features=[
+            feature.Feature("id", feature_group_id=12),
+            feature.Feature("tf1_name", feature_group_id=12),
+        ],
+        id=12,
+        stream=False,
+    )
+
+    fg3 = feature_group.FeatureGroup(
+        name="test3",
+        version=1,
+        featurestore_id=99,
+        primary_key=[],
+        partition_key=[],
+        features=[
+            feature.Feature("id", feature_group_id=13),
+            feature.Feature("tf_name", feature_group_id=13),
+            feature.Feature("tf1_name", feature_group_id=13),
+            feature.Feature("tf3_name", feature_group_id=13),
+        ],
+        id=13,
+        stream=False,
+    )
+
     def test_from_response_json_python(self, mocker, backend_fixtures):
         # Arrange
         mocker.patch("hsfs.engine.get_type", return_value="python")
@@ -155,11 +200,170 @@ class TestQuery:
 
     def test_collect_feature(self, mocker, backend_fixtures):
         mocker.patch("hsfs.engine.get_type", return_value="python")
-        q = query.Query.from_response_json(backend_fixtures["query"]["get"]["response"])
 
-        features = q._collect_features()
+        # Act
+        q = TestQuery.fg1.select(["label"]).join(TestQuery.fg2.select(["tf1_name"]))
+
+        features = q.features
         feature_names = [feature.name for feature in features]
 
-        expected_feature_names = ["test_left_features", "test_left_features2"]
-        assert len(feature_names) == len(expected_feature_names)
-        assert feature_names == expected_feature_names
+        expected_features = [TestQuery.fg1["label"], TestQuery.fg2["tf1_name"]]
+        expected_feature_names = ["label", "tf1_name"]
+
+        # Assert
+        assert len(feature_names) == len(expected_features)
+        for i, feat in enumerate(expected_features):
+            assert feat.name == expected_feature_names[i]
+
+    def test_collect_featuregroups(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = (
+            TestQuery.fg1.select(["label"])
+            .join(TestQuery.fg2.select(["tf1_name"]))
+            .join(TestQuery.fg2.select(["tf1_name"]))
+        )
+        expected_featuregroups = [TestQuery.fg1, TestQuery.fg2]
+
+        # Assert
+        assert len(q.featuregroups) == len(expected_featuregroups)
+        assert set(q.featuregroups) == set(expected_featuregroups)
+
+    def test_append_feature(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = TestQuery.fg1.select([TestQuery.fg1["label"]]).append_feature("id")
+        expected_features = [TestQuery.fg1["label"], feature.Feature("id")]
+
+        # Assert
+        assert len(q.features) == len(expected_features)
+        for i, feat in enumerate(expected_features):
+            assert feat.name == expected_features[i].name
+
+    def test_get_feature(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = TestQuery.fg1.select(TestQuery.fg1["label"]).join(
+            TestQuery.fg2.select(TestQuery.fg2["tf1_name"])
+        )
+
+        # Assert
+        assert id(q.get_feature("tf1_name")) == id(
+            TestQuery.fg2.get_feature("tf1_name")
+        )
+
+    def test_get_index(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = TestQuery.fg1.select(TestQuery.fg1["label"]).join(
+            TestQuery.fg2.select(TestQuery.fg2["tf1_name"])
+        )
+
+        # Assert
+        assert id(q.get_feature("tf1_name")) == id(
+            TestQuery.fg2.get_feature("tf1_name")
+        )
+
+    def test_get_attr(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = TestQuery.fg1.select(TestQuery.fg1["label"]).join(
+            TestQuery.fg2.select(TestQuery.fg2["tf1_name"])
+        )
+
+        # Assert
+        assert id(q.get_feature("tf1_name")) == id(
+            TestQuery.fg2.get_feature("tf1_name")
+        )
+
+    def test_get_feature_by_name(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = (
+            TestQuery.fg1.select_all()
+            .join(TestQuery.fg2.select_all())
+            .join(TestQuery.fg3.select_all())
+        )
+
+        # Assert
+        assert (
+            q._get_feature_by_name("tf3_name")[0].name == TestQuery.fg3["tf3_name"].name
+        )
+        assert (
+            q._get_feature_by_name("tf3_name")[0].feature_group_id
+            == TestQuery.fg3["tf3_name"].feature_group_id
+        )
+
+    def test_get_feature_by_name_prefix(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = (
+            TestQuery.fg1.select_all()
+            .join(TestQuery.fg2.select_all())
+            .join(TestQuery.fg3.select_all(), prefix="fg3")
+        )
+
+        # Assert
+        assert (
+            q._get_feature_by_name("tf_name")[0].name == TestQuery.fg1["tf_name"].name
+        )
+        assert (
+            q._get_feature_by_name("tf_name")[0].feature_group_id
+            == TestQuery.fg1["tf_name"].feature_group_id
+        )
+
+    def test_get_feature_by_name_ambiguous(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = (
+            TestQuery.fg1.select_all()
+            .join(TestQuery.fg2.select_all())
+            .join(TestQuery.fg3.select_all(), prefix="fg3")
+        )
+
+        # Assert
+        with pytest.raises(FeatureStoreException) as e_info:
+            q._get_feature_by_name("id")[0]
+
+        assert str(e_info.value) == query.Query.ERROR_MESSAGE_FEATURE_AMBIGUOUS.format(
+            "id"
+        )
+
+    def test_get_feature_by_feature_ambiguous(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = (
+            TestQuery.fg1.select_all()
+            .join(TestQuery.fg2.select_all())
+            .join(TestQuery.fg3.select_all(), prefix="fg3")
+        )
+
+        # Assert
+        with pytest.raises(FeatureStoreException) as e_info:
+            q._get_featuregroup_by_feature(feature.Feature("id"))[0]
+
+        assert str(
+            e_info.value
+        ) == query.Query.ERROR_MESSAGE_FEATURE_AMBIGUOUS_FG.format("id")
+
+    def test_get_feature_by_feature_non_ambiguous(self, mocker, backend_fixtures):
+        mocker.patch("hsfs.engine.get_type", return_value="python")
+
+        # Act
+        q = (
+            TestQuery.fg1.select_all()
+            .join(TestQuery.fg2.select_all())
+            .join(TestQuery.fg3.select_all(), prefix="fg3")
+        )
+
+        # Assert
+        assert q._get_featuregroup_by_feature(TestQuery.fg3["id"]) == TestQuery.fg3
