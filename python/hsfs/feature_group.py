b06642c1a9f7f846d69e71f72bc4ab39dbe19f83
bubriks <bubriks@gmail.com>
2023-07-18 15:50:34 +0200

    Update feature_group.py

--- python/hsfs/feature_group.py
+++ python/hsfs/feature_group.py
@@ -73,7 +73,7 @@ class FeatureGroupBase:
         self._location = location
         self._id = id
         self._subject = None
-        self._online_topic_name = online_topic_name
+        self._online_topic_name = online_topic_name  # same for the whole project
         self._feature_store_id = featurestore_id
         # use setter for correct conversion
         self.expectation_suite = expectation_suite
