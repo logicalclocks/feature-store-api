4fc75a5848aa267c5b5289074be73717c6596ec0
bubriks <bubriks@gmail.com>
2023-08-09 13:35:04 +0200

    Update query.py

--- python/hsfs/constructor/query.py
+++ python/hsfs/constructor/query.py
@@ -70,7 +70,6 @@ class Query:
         )
 
         if self._left_feature_group:
-            raise FeatureStoreException("1111")
             self._left_feature_group.check_deprecated()
 
     def _prep_read(self, online, read_options):
