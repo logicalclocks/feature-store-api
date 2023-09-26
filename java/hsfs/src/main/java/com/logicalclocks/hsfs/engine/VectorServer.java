7fb464d4b5f46731c15cd6654d52676fba91922a
davitbzh <davit.bzhalava@gmail.com>
2023-09-26 11:25:45 +0200

    ken bug fix

--- java/hsfs/src/main/java/com/logicalclocks/hsfs/engine/VectorServer.java
+++ java/hsfs/src/main/java/com/logicalclocks/hsfs/engine/VectorServer.java
@@ -52,6 +52,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -212,8 +213,12 @@ public class VectorServer {
     for (Integer fgId : preparedQueryString.keySet()) {
       String query = preparedQueryString.get(fgId);
       String zippedTupleString =
-          zipArraysToTupleString(preparedStatementParameters.get(fgId).keySet().stream().map(entry::get)
-              .collect(Collectors.toList()));
+          zipArraysToTupleString(preparedStatementParameters.get(fgId)
+            .entrySet()
+            .stream()
+            .sorted(Comparator.comparingInt(Map.Entry::getValue))
+            .map(e -> entry.get(e.getKey()))
+            .collect(Collectors.toList()));
       queries.add(query.replaceFirst("\\?", zippedTupleString));
     }
     return getFeatureVectors(featureStoreBase, features, queries, external);
