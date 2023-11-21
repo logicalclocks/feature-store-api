cd9d7110f73bffa446fc233b783c5cb6b5bea356
MKLepium <maximilian.kurzawski@gmail.com>
2023-11-22 00:18:53 +0100

    added strip to authentication headers

--- python/hsfs/client/auth.py
+++ python/hsfs/client/auth.py
@@ -21,7 +21,7 @@ class BearerAuth(requests.auth.AuthBase):
     """Class to encapsulate a Bearer token."""
 
     def __init__(self, token):
-        self._token = token
+        self._token = token.strip()
 
     def __call__(self, r):
         r.headers["Authorization"] = "Bearer " + self._token
@@ -32,7 +32,7 @@ class ApiKeyAuth(requests.auth.AuthBase):
     """Class to encapsulate an API key."""
 
     def __init__(self, token):
-        self._token = token
+        self._token = token.strip()
 
     def __call__(self, r):
         r.headers["Authorization"] = "ApiKey " + self._token
