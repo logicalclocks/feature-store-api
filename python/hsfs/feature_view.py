60bfb1a74811b323ed014afa40fc9468e5d23053
manu-sj <152865565+manu-sj@users.noreply.github.com>
2023-12-14 11:44:52 +0100

    Correcting default value for write_options in recreate_training_data (#1166)

--- python/hsfs/feature_view.py
+++ python/hsfs/feature_view.py
@@ -1656,7 +1656,7 @@ class FeatureView:
     def recreate_training_dataset(
         self,
         training_dataset_version: int,
-        write_options: Optional[Dict[Any, Any]] = None,
+        write_options: Optional[Dict[Any, Any]] = {},
         spine: Optional[
             Union[
                 pd.DataFrame,
