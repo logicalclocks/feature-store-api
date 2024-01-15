f202c78528ab6009b74e4435bca3938ba8790472
Javier de la Rúa Martínez <javier@logicalclocks.com>
2024-01-15 10:30:15 +0100

    Update license

--- java/hsfs/src/main/java/com/logicalclocks/hsfs/metadata/FeatureDescriptiveStatistics.java
+++ java/hsfs/src/main/java/com/logicalclocks/hsfs/metadata/FeatureDescriptiveStatistics.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2020-2023. Hopsworks AB
+ *  Copyright (c) 2020-2024. Hopsworks AB
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
