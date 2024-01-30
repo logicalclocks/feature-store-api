0c30d8dce48a9e6795fe5cde5118ee454010f854
bubriks <bubriks@gmail.com>
2024-01-30 16:39:49 +0100

    fix setting notification_topic_name

--- python/hsfs/core/feature_group_engine.py
+++ python/hsfs/core/feature_group_engine.py
@@ -225,7 +225,7 @@ class FeatureGroupEngine(feature_group_base_engine.FeatureGroupBaseEngine):
     def update_notification_topic_name(self, feature_group, notification_topic_name):
         """Updates the notification_topic_name of a feature group."""
         copy_feature_group = fg.FeatureGroup.from_response_json(feature_group.to_dict())
-        copy_feature_group.description = notification_topic_name
+        copy_feature_group.notification_topic_name = notification_topic_name
         self._feature_group_api.update_metadata(
             feature_group, copy_feature_group, "updateMetadata"
         )
