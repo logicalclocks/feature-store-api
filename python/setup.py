60c130a4020889b5fdd5544f2129e006d21cf257
Ralf <bubriks@gmail.com>
2024-01-30 13:16:36 +0200

    [FSTORE-1193] HSFS python tests fail after moto has been updated (#1206)
    (cherry picked from commit cd784901662f3edbcf22bf1cb16d179ee5b5a945)

--- python/setup.py
+++ python/setup.py
@@ -42,7 +42,7 @@ setup(
             "flake8",
             "black",
             "pyspark==3.1.1",
-            "moto[s3]",
+            "moto[s3]==5.0.0",
         ],
         "dev-pandas1": [
             "pytest==7.1.2",
@@ -50,7 +50,7 @@ setup(
             "flake8",
             "black",
             "pyspark==3.1.1",
-            "moto[s3]",
+            "moto[s3]==5.0.0",
             "pandas<=1.5.3",
             "sqlalchemy<=1.4.48",
         ],
