1d4eee44fac3ac89247ec4e9e523b8c8b4588c91
bubriks <bubriks@gmail.com>
2024-02-07 10:32:04 +0100

    add db type test

--- python/tests/test_util.py
+++ python/tests/test_util.py
@@ -86,3 +86,19 @@ class TestUtil:
     def test_convert_hudi_commit_time_to_timestamp(self):
         timestamp = util.get_timestamp_from_date_string("20221118095233099")
         assert timestamp == 1668765153099
+
+    def test_get_dataset_type_HIVEDB(self):
+        db_type = util.get_dataset_type("/apps/hive/warehouse/temp_featurestore.db/storage_connector_resources/kafka__tstore.jks")
+        assert db_type == "HIVEDB"
+
+    def test_get_dataset_type_HIVEDB_with_dfs(self):
+        db_type = util.get_dataset_type("dfs:///apps/hive/warehouse/temp_featurestore.db/storage_connector_resources/kafka__tstore.jks")
+        assert db_type == "HIVEDB"
+    
+    def test_get_dataset_type_DATASET(self):
+        db_type = util.get_dataset_type("/Projects/temp/Resources/kafka__tstore.jks")
+        assert db_type == "DATASET"
+
+    def test_get_dataset_type_DATASET_with_dfs(self):
+        db_type = util.get_dataset_type("dfs:///Projects/temp/Resources/kafka__tstore.jks")
+        assert db_type == "DATASET"
