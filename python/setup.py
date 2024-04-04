bd4ec0c887a285c9fb0a77b6617c29c81f60dad5
manu-sj <manu.joseph@logicalclocks.com>
2024-04-04 14:32:31 +0200

    pinning polars version to between 0.20.18 and 0.21.0

--- python/setup.py
+++ python/setup.py
@@ -36,7 +36,7 @@ setup(
         "fsspec",
         "retrying",
         "aiomysql",
-        "polars==0.20.18",
+        "polars>=0.20.18,<0.21.0",
         "opensearch-py>=1.1.0,<=2.4.2",
     ],
     extras_require={
