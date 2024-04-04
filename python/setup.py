fb0cd76fd1e4caee8d9160631a87a9e2e44e3eab
manu-sj <manu.joseph@logicalclocks.com>
2024-04-04 14:10:29 +0200

    pinning polars to stable release version

--- python/setup.py
+++ python/setup.py
@@ -36,7 +36,7 @@ setup(
         "fsspec",
         "retrying",
         "aiomysql",
-        "polars==0.20.0",
+        "polars==0.20.18",
         "opensearch-py>=1.1.0,<=2.4.2",
     ],
     extras_require={
