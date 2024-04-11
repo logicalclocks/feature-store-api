0176166ad0484f392afce9e840dbb06fa0932472
Victor Jouffrey <vicatjou@gmail.com>
2024-04-12 00:42:14 +0200

    Fix documentation dependency issues

--- docs/js/inject-api-links.js
+++ docs/js/inject-api-links.js
@@ -1,14 +1,22 @@
 window.addEventListener("DOMContentLoaded", function () {
     var windowPathNameSplits = window.location.pathname.split("/");
     var majorVersionRegex = new RegExp("(\\d+[.]\\d+)")
+    var latestRegex = new RegExp("latest");
     if (majorVersionRegex.test(windowPathNameSplits[1])) { // On landing page docs.hopsworks.api/3.0 - URL contains major version
         // Version API dropdown
         document.getElementById("hopsworks_api_link").href = "https://docs.hopsworks.ai/hopsworks-api/" + windowPathNameSplits[1] + "/generated/api/login/";
         document.getElementById("hsfs_api_link").href = "https://docs.hopsworks.ai/feature-store-api/" + windowPathNameSplits[1] + "/generated/api/connection_api/";
         document.getElementById("hsml_api_link").href = "https://docs.hopsworks.ai/machine-learning-api/" + windowPathNameSplits[1] + "/generated/connection_api/";
     } else { // on docs.hopsworks.api/feature-store-api/3.0 / docs.hopsworks.api/hopsworks-api/3.0 / docs.hopsworks.api/machine-learning-api/3.0
-        var apiVersion = windowPathNameSplits[2];
-        var majorVersion = apiVersion.match(majorVersionRegex)[0];
+        if (latestRegex.test(windowPathNameSplits[2]) || latestRegex.test(windowPathNameSplits[1])) {
+          console.log("latest version");
+          var majorVersion = "latest";
+        } else {
+
+          var apiVersion = windowPathNameSplits[2];
+          var majorVersion = apiVersion.match(majorVersionRegex)[0];
+          console.log("specific version", majorVersion);
+        }
         // Version main navigation
         document.getElementsByClassName("md-tabs__link")[0].href = "https://docs.hopsworks.ai/" + majorVersion;
         document.getElementsByClassName("md-tabs__link")[1].href = "https://colab.research.google.com/github/logicalclocks/hopsworks-tutorials/blob/master/quickstart.ipynb";
