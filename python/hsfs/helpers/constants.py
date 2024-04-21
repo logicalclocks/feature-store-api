faa1add4ddf57b97a313cbb1eda8728fba349510
Victor Jouffrey <vicatjou@gmail.com>
2024-04-21 16:12:37 +0200

    Scaffold some structure for python SDK improved communication

---
+++ python/hsfs/helpers/constants.py
@@ -0,0 +1,20 @@
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+HOPSWORKS_VERBOSE_ENV_VAR = "HOPSWORKS_VERBOSE"
+
+USE_RICH_LOGGER_ENV_VAR = "HOPSWORKS_USE_RICH_LOGGER"
+USE_RICH_CONSOLE_ENV_VAR = "HOPSWORKS_USE_RICH_CONSOLE"
+DEFAULT_VERBOSE_CONFIG = {
+    "tab_size": 2,
+    "width": 120,
+    "color_system": "truecolor",
+}
---
+++ python/hsfs/helpers/logger.py
@@ -0,0 +1,33 @@
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+from __future__ import annotations
+
+import logging
+import os
+
+
+def is_rich_logger_enabled():
+    return os.getenv("HOPSWORKS_USE_RICH_LOGGER", "true").lower() == "true"
+
+
+def set_rich_for_hsfs_root_logger():
+    logger = logging.getLogger("hsfs")
+    if is_rich_logger_enabled():
+        from rich.logging import RichHandler
+
+        rich_handler = RichHandler(
+            rich_tracebacks=True,
+            tracebacks_show_locals=True,
+            tracebacks_show_hidden_frames=True,
+        )
+        logger.addHandler(rich_handler)
+    return logger
