6c801ee3f3623b7130d06ecd37b026e9c5ce86e4
manu-sj <manu.joseph@logicalclocks.com>
2024-05-04 14:43:49 +0200

    reformatted and added unit tests for feature view

--- python/hsfs/feature_view.py
+++ python/hsfs/feature_view.py
@@ -123,19 +123,14 @@ class FeatureView:
             training_helper_columns if training_helper_columns else []
         )
 
-        # TODO : Clean this up
-        if transformation_functions:
-            for i, transformation_function in enumerate(transformation_functions):
-                if not isinstance(transformation_function, TransformationFunction):
-                    transformation_functions[i] = TransformationFunction(
-                        self.featurestore_id,
-                        hopsworks_udf=transformation_function,
-                        version=1,
-                    )
-
-        self._transformation_functions: List[TransformationFunction] = (
-            transformation_functions
-        )
+        self._transformation_functions: List[TransformationFunction] = [
+            TransformationFunction(
+                self.featurestore_id, hopsworks_udf=transformation_function, version=1
+            )
+            if not isinstance(transformation_function, TransformationFunction)
+            else transformation_function
+            for transformation_function in transformation_functions
+        ]
 
         self._features = []
         self._feature_view_engine: feature_view_engine.FeatureViewEngine = (
@@ -3342,6 +3337,14 @@ class FeatureView:
 
     @classmethod
     def from_response_json(cls, json_dict: Dict[str, Any]) -> "FeatureView":
+        """
+        Function that constructs the class object from its json serialization.
+
+        # Arguments
+            json_dict: `Dict[str, Any]`. Json serialized dictionary for the class.
+        # Returns
+            `TransformationFunction`: Json deserialized class object.
+        """
         json_decamelized = humps.decamelize(json_dict)
 
         serving_keys = json_decamelized.get("serving_keys", None)
@@ -3349,6 +3352,7 @@ class FeatureView:
             serving_keys = [
                 skm.ServingKey.from_response_json(sk) for sk in serving_keys
             ]
+        transformation_functions = json_decamelized.get("transformation_functions", {})
         fv = cls(
             id=json_decamelized.get("id", None),
             name=json_decamelized["name"],
@@ -3358,12 +3362,11 @@ class FeatureView:
             description=json_decamelized.get("description", None),
             featurestore_name=json_decamelized.get("featurestore_name", None),
             serving_keys=serving_keys,
-            transformation_functions=[
-                TransformationFunction.from_response_json(transformation)
-                for transformation in json_decamelized.get(
-                    "transformation_functions", []
-                )
-            ],
+            transformation_functions=TransformationFunction.from_response_json(
+                transformation_functions
+            )
+            if transformation_functions
+            else [],
         )
         features = json_decamelized.get("features", [])
         if features:
@@ -3385,6 +3388,14 @@ class FeatureView:
         return fv
 
     def update_from_response_json(self, json_dict: Dict[str, Any]) -> "FeatureView":
+        """
+        Function that updates the class object from its json serialization.
+
+        # Arguments
+            json_dict: `Dict[str, Any]`. Json serialized dictionary for the class.
+        # Returns
+            `TransformationFunction`: Json deserialized class object.
+        """
         other = self.from_response_json(json_dict)
         for key in [
             "name",
@@ -3426,9 +3437,21 @@ class FeatureView:
         )
 
     def json(self) -> str:
+        """
+        Convert class into its json serialized form.
+
+        # Returns
+            `str`: Json serialized object.
+        """
         return json.dumps(self, cls=util.FeatureStoreEncoder)
 
     def to_dict(self) -> Dict[str, Any]:
+        """
+        Convert class into a dictionary.
+
+        # Returns
+            `Dict`: Dictionary that contains all data required to json serialize the object.
+        """
         return {
             "featurestoreId": self._featurestore_id,
             "name": self._name,
