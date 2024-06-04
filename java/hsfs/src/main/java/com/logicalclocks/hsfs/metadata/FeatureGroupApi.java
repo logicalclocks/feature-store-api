00d081bcafd2c214039258a6503f51b4a3d50dbe
bubriks <bubriks@gmail.com>
2024-06-04 17:00:34 +0200

    fix java

--- java/hsfs/src/main/java/com/logicalclocks/hsfs/metadata/FeatureGroupApi.java
+++ java/hsfs/src/main/java/com/logicalclocks/hsfs/metadata/FeatureGroupApi.java
@@ -300,8 +300,7 @@ public class FeatureGroupApi {
   private <T extends FeatureGroupBase> void checkFeatures(T fg) {
     if (fg.getFeatures() == null || fg.getFeatures().isEmpty()) {
       LOGGER.warn(String.format("Feature Group `%s`, version `%s` has no features " +
-          "(to resolve this issue contact the admin or delete and recreate the feature group)",
-        fg.getName(), fg.getVersion()));
+          "(to resolve this issue contact the admin or delete and recreate the feature group)", fg.getName(), fg.getVersion()));
     }
   }
 }
