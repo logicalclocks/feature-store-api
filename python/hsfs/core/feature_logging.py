b4e059266a27e024ce28f49e3b2e350c9a451546
kenneth <kenneth@logicalclocks.com>
2024-07-05 11:26:21 +0200

    fix circular import

--- python/hsfs/core/feature_logging.py
+++ python/hsfs/core/feature_logging.py
@@ -2,27 +2,28 @@ import json
 from typing import Any, Dict
 
 import humps
-from hsfs import feature_group, util
-
+import hsfs.feature_group
+from hsfs import util
 
 class FeatureLogging:
 
     def __init__(self, id: int,
-                 transformed_features: feature_group.FeatureGroup,
-                 untransformed_features: feature_group.FeatureGroup):
+                 transformed_features: "feature_group.FeatureGroup",
+                 untransformed_features: "feature_group.FeatureGroup"):
         self._id = id
         self._transformed_features = transformed_features
         self._untransformed_features = untransformed_features
 
     @classmethod
     def from_response_json(cls, json_dict: Dict[str, Any]) -> 'FeatureLogging':
+        from hsfs.feature_group import FeatureGroup  # avoid circular import
         json_decamelized = humps.decamelize(json_dict)
         transformed_features = json_decamelized.get('transformed_log')
         untransformed_features = json_decamelized.get('untransformed_log')
         if transformed_features:
-            transformed_features = feature_group.FeatureGroup.from_response_json(transformed_features)
+            transformed_features = FeatureGroup.from_response_json(transformed_features)
         if untransformed_features:
-            untransformed_features = feature_group.FeatureGroup.from_response_json(untransformed_features)
+            untransformed_features = FeatureGroup.from_response_json(untransformed_features)
         return cls(json_decamelized.get('id'), transformed_features, untransformed_features)
 
     @property
