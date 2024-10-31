d189732cccb96c72e87cc46a385a0192cc353785
bubriks <bubriks@gmail.com>
2024-10-31 10:53:38 +0200

    init

--- utils/python/hsfs_utils.py
+++ utils/python/hsfs_utils.py
@@ -7,6 +7,7 @@ from typing import Any, Dict
 
 import hsfs
 from hsfs import engine
+from hsfs.engine import python
 from hsfs.constructor import query
 from hsfs.core import (
     feature_monitoring_config_engine,
@@ -280,7 +281,13 @@ def offline_fg_materialization(
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         # if all else fails read from the beggining
-        offset_string = "earliest"
+        initial_check_point_string = python.Engine().kafka_get_offsets(
+            topic_name=entity._online_topic_name,
+            feature_store_id=entity.feature_store_id,
+            offline_write_options={},
+            high=False,
+        )
+        offset_string = json.dumps(_build_starting_offsets(initial_check_point_string))
     print(f"startingOffsets: {offset_string}")
 
     # read kafka topic
