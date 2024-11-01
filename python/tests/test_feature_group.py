73e600156a8015aad2ec8d511bca8332196c6e34
Fabio Buso <fabio@hopsworks.ai>
2024-11-01 11:09:14 +0100

    [FSTORE-1593] S3 Connector doesn't refetch credentials when preparing Spark write (#1401)
    * [FSTORE-1593] S3 Connector doesn't refetch credentials when preparing Spark write
    
    * Release 3.9.0-RC4

--- python/tests/test_feature_group.py
+++ python/tests/test_feature_group.py
@@ -904,6 +904,7 @@ class TestExternalFeatureGroup:
     def test_prepare_spark_location_with_s3_connector(self, mocker, backend_fixtures):
         # Arrange
         engine = spark.Engine()
+        refetch_api = mocker.patch("hsfs.storage_connector.S3Connector.refetch")
         engine_instance = mocker.patch("hsfs.engine.get_instance", return_value=engine)
         json = backend_fixtures["feature_group"]["get_basic_info"]["response"]
         fg = feature_group.FeatureGroup.from_response_json(json)
@@ -918,6 +919,7 @@ class TestExternalFeatureGroup:
         # Assert
         assert fg.location == path
         engine_instance.assert_called_once()
+        refetch_api.assert_called_once()
 
     def test_prepare_spark_location_with_s3_connector_python(
         self, mocker, backend_fixtures
@@ -925,6 +927,7 @@ class TestExternalFeatureGroup:
         # Arrange
         engine = python.Engine()
         engine_instance = mocker.patch("hsfs.engine.get_instance", return_value=engine)
+        mocker.patch("hsfs.storage_connector.S3Connector.refetch")
         json = backend_fixtures["feature_group"]["get_basic_info"]["response"]
         fg = feature_group.FeatureGroup.from_response_json(json)
         fg._location = f"{fg.name}_{fg.version}"
