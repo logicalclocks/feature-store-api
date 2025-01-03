60e39eceb6ec0a2629c44c58162a327ab638c161
bubriks <bubriks@gmail.com>
2025-01-03 17:17:21 +0200

    tt

--- python/hsfs/core/deltastreamer_jobconf.py
+++ python/hsfs/core/deltastreamer_jobconf.py
@@ -29,7 +29,7 @@ class DeltaStreamerJobConf:
     def to_dict(self):
         return {
             "writeOptions": self._options,
-            JobConfiguration.DTO_TYPE: JobConfiguration(**self._spark_options).to_dict(),
+            JobConfiguration.DTO_TYPE: JobConfiguration(**self._spark_options if self._spark_options else {}).to_dict(),
         }
 
     def json(self):
--- python/hsfs/core/training_dataset_job_conf.py
+++ python/hsfs/core/training_dataset_job_conf.py
@@ -18,6 +18,7 @@ from __future__ import annotations
 import json
 
 from hsfs import util
+from hsfs.core.job_configuration import JobConfiguration
 
 
 class TrainingDatasetJobConf:
@@ -71,5 +72,5 @@ class TrainingDatasetJobConf:
             ]
             if self._write_options
             else None,
-            "sparkJobConfiguration": self._spark_job_configuration,
+            JobConfiguration.DTO_TYPE: self._spark_job_configuration,
         }
