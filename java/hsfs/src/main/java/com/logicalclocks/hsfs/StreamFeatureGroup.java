b55b9923822bd74c8647c6aeae1f51dd7d3b1fa3
davitbzh <davit.bzhalava@gmail..com>
2025-01-20 18:45:15 +0100

    licence headers

--- java/hsfs/src/main/java/com/logicalclocks/hsfs/StreamFeatureGroup.java
+++ java/hsfs/src/main/java/com/logicalclocks/hsfs/StreamFeatureGroup.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2023. Hopsworks AB
+ *  Copyright (c) 2025. Hopsworks AB
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
